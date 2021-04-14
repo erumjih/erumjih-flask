a73da13c5788a52824c54f8a9e8f52b98c9336f9
정일훈 <erumjih@gmail.com>
2021-04-14 21:19:11 +0900

    log 수정

--- config/production.py
+++ config/production.py
@@ -9,14 +9,14 @@ dictConfig({
     'version': 1,
     'formatters': {
         'default': {
-            'format': '[%(asctime)s %(levelname)s in %(module)s: %(message)s',
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
         }
     },
     'handlers': {
         'file': {
             'level': 'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, 'log/myproject.log'),
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
             'maxBytes': 1024 * 1024 * 5, # 5MB
             'backupCount': 5,
             'formatter': 'default',
